a5beae790ba00f66f083e2faf324720b1003f99a
Gianfranco Silvaledesma <gianfrancosilvaledesma@gmail.com>
2025-09-18 23:00:28 -0300

    ultimos cambios

--- PrimeraClaseReact/src/components/NavBar/NavBar.jsx
+++ PrimeraClaseReact/src/components/NavBar/NavBar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./NavBar.css"
 import CartWidget from '../CartWidget/CartWidget'
+import { Link } from "react-router-dom"
 
 const NavBar = () => {
   return (
@@ -9,7 +10,9 @@ const NavBar = () => {
         <h1 className='texto'>Cerveceria LA CARBONERA</h1>
         <nav>
             <ul>
-                <li>Cervezas</li>
+                <li>
+                  <Link to="/">Cervezas</Link>
+                  </li>
                 <li>Nosotros</li>
                 <li>Contacto</li>
             </ul>
